e12949b71678da46a330c02b940701196d7f4cf5
Lucas Pessoa <lucaspessoalves25@gmail.com>
2024-06-16 18:31:37 -0300

    Update PagInicialController.java

--- src/main/java/tela/PagInicialController.java
+++ src/main/java/tela/PagInicialController.java
@@ -9,7 +9,6 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import modelos.Produto;
-
 import java.util.List;
 import java.util.ArrayList;
 
@@ -21,17 +20,10 @@ public class PagInicialController {
     @FXML
     private TextField txtPesquisa;
 
-
-
     private static final int ITEMS_PER_PAGE = 10;
 
     @FXML
     private Pagination pagination;
-
-    private ObservableList<Produto> allProducts;
-
-    
-
 
     @FXML
     protected void removerProdutoSelecionado(){
@@ -75,38 +67,44 @@ public class PagInicialController {
 
         tvProdutos.getColumns().addAll(colNome, colDescricao, colMarca, colCategoria, colPreco, colCusto);
 
+        pagination.currentPageIndexProperty().addListener((obs, oldIndex, newIndex) -> atualizaTabela(newIndex.intValue()));
         atualiza();
+        atualizaTabela(0);
     }
 
     private void atualizaTabela(int pageIndex) {
+        List<Produto> produtos = Loja.showTable();
         int start = pageIndex * ITEMS_PER_PAGE;
-        int end = Math.min(start + ITEMS_PER_PAGE, allProducts.size());
-        tvProdutos.getItems().setAll(allProducts.subList(start, end));
+        int end = Math.min(start + ITEMS_PER_PAGE, produtos.size());
+        if (start <= end) {
+            tvProdutos.getItems().setAll(produtos.subList(start, end));
+        }
     }
 
     public void atualiza() {
-        tvProdutos.getItems().clear();
         List<Produto> produtos = Loja.showTable();
-        tvProdutos.getItems().addAll(produtos);
 
-        // Inicializa a lista de produtos
-        allProducts = FXCollections.observableArrayList(Loja.showTable());
         // Configura a paginação
-        int pageCount = (int) Math.ceil((double) allProducts.size() / ITEMS_PER_PAGE);
+        int pageCount = (int) Math.ceil((double) produtos.size() / ITEMS_PER_PAGE);
         pagination.setPageCount(pageCount);
-        pagination.currentPageIndexProperty().addListener((obs, oldIndex, newIndex) -> atualizaTabela(newIndex.intValue()));
+
+        if (pagination.getCurrentPageIndex() >= pageCount) {
+            pagination.setCurrentPageIndex(pageCount - 1);
+        } else {
+            atualizaTabela(pagination.getCurrentPageIndex());
+        }
     }
+
     @FXML
     public void pesquisa() {
         String nomeProduto = txtPesquisa.getText();
-        if (nomeProduto == null || nomeProduto.trim().isEmpty()){
+        if (nomeProduto == null || nomeProduto.trim().isEmpty()) {
             atualiza();
         } else {
             List<Produto> produtos = Loja.showTable();
             List<Produto> produtosFiltrados = new ArrayList<>();
 
             for (Produto p : produtos) {
-
                 String nomeProdutoMinusculo = nomeProduto.toLowerCase();
                 String nomeProdutoTabela = p.getNome().toLowerCase();
 
@@ -117,13 +115,21 @@ public class PagInicialController {
 
             tvProdutos.getItems().clear();
             tvProdutos.getItems().addAll(produtosFiltrados);
+
+            // Atualiza a paginação para os produtos filtrados
+            int pageCount = (int) Math.ceil((double) produtosFiltrados.size() / ITEMS_PER_PAGE);
+            pagination.setPageCount(pageCount);
+            pagination.setCurrentPageIndex(0); // Reseta para a primeira página
+
+            if (produtosFiltrados.size() > 0) {
+                atualizaTabela(0);
+            }
         }
     }
 
-
     @FXML
     protected void changeScreenToCadastrar(){
-        Main.chanceScreen("pagCadastro", null);
+        Main.chanceScreen("pagCadastro",null );
     }
     @FXML
     protected void changeScreenToEdicao(){
@@ -141,8 +147,5 @@ public class PagInicialController {
             alert.setContentText("Por favor, selecione algum item para editar.");
             alert.showAndWait();
         }
-
     }
-
-
 }
